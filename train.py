solved empty gradient bug

train.py
@@ -1,8 +1,8 @@
 import torch
-from torch import nn, optim
+from torch import autograd, clone, nn, optim, cuda
 
-from torch.utils import save_experiment, save_checkpoint
-from torch.data import prepare_data
+from utils import save_experiment, save_checkpoint
+from data import prepare_data
 from vit import ViTForClassification
 
 config = {
@@ -39,7 +39,7 @@ class Trainer:
       
       #Train the model
       for i in range(epochs):
-         train_loss = self.train_epochs[trainloader]
+         train_loss = self.train_epoch(trainloader)
          accuracy, test_loss = self.evaluate(testloader)
          train_losses.append(train_losses)
          test_losses.append(test_loss)
@@ -48,9 +48,10 @@ class Trainer:
          if save_model_every_n_epochs > 0 and (i+1) % save_model_every_n_epochs == 0 and i+1 != epochs:
             print('\tSave checkpoint at epoch', i+1)
             save_checkpoint(self.exp_name, self.model, i+1)
-      save_experiment(self.exp_name, self.model, train_losses, test_losses, accuracies)
+      save_experiment(self.exp_name, config, self.model, train_losses, test_losses, accuracies)
          
    def train_epoch(self, trainloader):
+      torch.autograd.set_detect_anomaly(True)
       self.model.train()
       total_loss = 0
       for batch in trainloader:
@@ -58,9 +59,10 @@ class Trainer:
          images, labels = batch
          self.optimizer.zero_grad()
          loss = self.loss_fn(self.model(images)[0], labels)
+         loss = clone(loss) 
          loss.backward()
          self.optimizer.step()
-         total_loss += loss.item() * len(images)
+         total_loss = total_loss + loss.item() * len(images)
       return total_loss / len(trainloader.dataset)
    
    @torch.no_grad()
@@ -76,7 +78,7 @@ class Trainer:
             logits, _ = self.model(images)
             
             loss = self.loss_fn(logits, labels)
-            total_loss += loss.items() * len(images)
+            total_loss = total_loss + loss.item() * len(images)
             
             predictions = torch.argmax(logits, dim=1)
             correct += torch.sum(predictions == labels).item()
@@ -97,13 +99,14 @@ def parse_args():
    args = parser.parse_args()
    if args.device is None:
       args.device = "cuda" if torch.cuda.is_available() else "cpu"
+   print(args.device) 
    return args
 
 def main():
     args = parse_args()
     batch_size = args.batch_size
     epochs = args.epochs
-    lr = epochs.lr
+    lr = args.lr
     device = args.device
     save_model_every_n_epochs = args.save_model_every
    
@@ -117,6 +120,5 @@ def main():
     trainer = Trainer(model, optimizer, loss_fn, args.exp_name, device=device)
     trainer.train(trainloader, testloader, epochs, save_model_every_n_epochs=save_model_every_n_epochs)
 
-"if __name__ == "__main__":
+if __name__ == "__main__":
     main()
-         

vit.py
@@ -1,6 +1,6 @@
 import math
 import torch
-from torch import nn
+from torch import clone, nn
 
 class NewGELUActivation(nn.Module):
     """
@@ -11,6 +11,7 @@ class NewGELUActivation(nn.Module):
     """
 
     def forward(self, input):
+        input = clone(input)
         return 0.5 * input * (1.0 + torch.tanh(math.sqrt(2.0 / math.pi) * (input + 0.044715 * torch.pow(input, 3.0))))
 
 class PatchEmbeddings(nn.Module):
@@ -52,11 +53,11 @@ class Embeddings(nn.Module):
         x = self.patch_embeddings(x)
         batch_size, _, _ = x.size()
 
-        classify_ts = self.classify_token.expand(batch_size, -1, -1)
+        classify_ts = self.classify_t.expand(batch_size, -1, -1)
 
         x = torch.cat((classify_ts, x), dim=1)
         x = x + self.position_embeddings
-        x = self.dropout(x)
+        x = self.Dropout(x)
         return x
 
 class AttentionHead(nn.Module):
@@ -85,9 +86,9 @@ class AttentionHead(nn.Module):
         #attention scores
         #softmax(Q*K.T/sqrt(head_size))*V
         attention_scores = torch.matmul(query, key.transpose(-1, -2))
-        attention_score = attention_scores / math.sqrt(self.attention_head_size)
+        attention_scores = attention_scores / math.sqrt(self.attention_head_size)
         attention_probs = nn.functional.softmax(attention_scores, dim=-1)
-        attention_probs = self.Dropout(attention_probs)
+        attention_probs = self.dropout(attention_probs)
 
         #calculate the attention output
         attention_output = torch.matmul(attention_probs, value)
@@ -107,7 +108,7 @@ class MultiHeadAttention(nn.Module):
         self.all_head_size = self.num_attention_heads * self.attention_head_size
 
         #to use bias or not in projections
-        self.qkv_bias = config["bias"]
+        self.qkv_bias = config["qkv_bias"]
         
         #Create a list of attention heads
         self.heads = nn.ModuleList([])
@@ -117,7 +118,7 @@ class MultiHeadAttention(nn.Module):
             
         #Creating a linear layer to project the attention output back to hidden size
         self.output_projection = nn.Linear(self.all_head_size, self.hidden_size)
-        self.output_dropout = nn.Dropout(config["hidden_output_prob"])
+        self.output_dropout = nn.Dropout(config["hidden_dropout_prob"])
 
     def forward(self, x, output_attentions=False):
         #Calculation attention for each attention head
@@ -166,11 +167,11 @@ class Block(nn.Module):
       attention_output, attention_probs = \
          self.attention(self.layernorm1(x), output_attentions=output_attentions)
       #skip connection
-      x += attention_output
+      x = x + attention_output
       #Feed forward network
       mlp_output = self.mlp(self.layernorm2(x))
       #skip connection
-      x += mlp_output
+      x = x + mlp_output
       #Returning the transformer's block output and the attention probabilities
       if not output_attentions:
          return(x, None)
@@ -231,3 +232,24 @@ class ViTForClassification(nn.Module):
          return(logits, None)
       else:
          return(logits, all_attentions)
+
+   def _init_weights(self, module):
+      if isinstance(module, (nn.Linear, nn.Conv2d)):
+        torch.nn.init.normal_(module.weight, mean=0.0, std=self.config["initializer_range"])
+        if module.bias is not None:
+            torch.nn.init.zeros_(module.bias)
+      elif isinstance(module, nn.LayerNorm):
+        module.bias.data.zero_()
+        module.weight.data.fill_(1.0)
+      elif isinstance(module, Embeddings):
+        module.position_embeddings.data = nn.init.trunc_normal_(
+             module.position_embeddings.data.to(torch.float32),
+             mean=0.0,
+             std=self.config["initializer_range"],
+            ).to(module.position_embeddings.dtype)
+
+        module.classify_t.data = nn.init.trunc_normal_(
+             module.classify_t.data.to(torch.float32),
+             mean=0.0,
+             std=self.config["initializer_range"],
+            ).to(module.classify_t.dtype)